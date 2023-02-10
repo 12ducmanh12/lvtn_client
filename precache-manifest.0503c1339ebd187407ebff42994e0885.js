d7b8a96016ad95185c226707771325eefa5c0dc8
Luong <luongnguyentrongduc@gmail.com>
2023-02-10 10:46:39 +0700

    Updates

---
+++ precache-manifest.0503c1339ebd187407ebff42994e0885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b182f30d6589591e6cb9",
+    "url": "/lvtn_client/static/css/main.26aaa3be.chunk.css"
+  },
+  {
+    "revision": "b182f30d6589591e6cb9",
+    "url": "/lvtn_client/static/js/main.b182f30d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/lvtn_client/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/lvtn_client/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "be576e9b5ad09b5b7a2d",
+    "url": "/lvtn_client/static/js/runtime~main.be576e9b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/lvtn_client/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "74243d36419fcef61657f0441f12dbf0",
+    "url": "/lvtn_client/index.html"
+  }
+];
